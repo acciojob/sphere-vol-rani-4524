917d45619cec84911ef6518e7744096b59ff37b3
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2025-11-22 16:03:56 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function volume_sphere() {
     //Write your code here
+	let radius = document.querySelector("#radius").value;
+	radius= Number(radius);
+	if(isNaN(radius) || radius<0){
+		document.getElementById("volume").value="NaN";
+		return false;
+	}
+
+	let sphere = (4 / 3) * Math.PI * Math.pow(radius, 3);
+	sphere=sphere.toFixed(4);
+	document.getElementById("volume").value=sphere;
+	return false;
+	
   
 } 
 
